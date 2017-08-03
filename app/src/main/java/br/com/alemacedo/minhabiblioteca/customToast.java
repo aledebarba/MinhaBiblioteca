4fa251521f5422d0d08587a560d02b1d69dd3dbc
logonrm <logonrm@fiap.com.br>
2017-08-02 21:56:08 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alemacedo/minhabiblioteca/customToast.java
@@ -0,0 +1,15 @@
+package br.com.alemacedo.minhabiblioteca;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+
+public class customToast {
+
+    public void showToast(Context context, String mensagem) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+}
